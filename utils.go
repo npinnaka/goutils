f80bfd33a83a1b46a567d9631db85e6d3deaa557
Narendra Pinnaka <learnnew@outlook.com>
2023-07-31 19:45:45 -0500

    Updated ReadME

--- utils.go
+++ utils.go
@@ -3,3 +3,15 @@ package utils
 func Sum(a, b int) int {
 	return a + b
 }
+
+func Diff(a, b int) int {
+	return a - b
+}
+
+func Bark() string {
+	return "Woff!!"
+}
+
+func Barks() string {
+	return "Woff Woff Woff!!"
+}
